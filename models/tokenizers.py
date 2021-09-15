b56ca6920fdea4e0c25c6183b084aa9b82042345
Alvaro Esperanca <alvaro.sdb.esperanca@gmail.com>
2021-09-15 15:16:00 -0400

    Change type hinting. Changed the type hinting to indicate that the tokenize method takes as input both a list of strings or a single string. Also added a check in the BiLSTM tokenizer to address single a single string input.

--- models/tokenizers.py
+++ models/tokenizers.py
@@ -1,17 +1,19 @@
-from typing import List
+from typing import List, Union
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from transformers import AutoTokenizer
 
+from models.vocab import Vocab
+
 
 class BaseTokenizer:
     """Base tokenizer class. All tokenizers inherit from this base class"""
 
-    def tokenize(self, corpus: List[str]):
+    def tokenize(self, corpus: Union[List[str], str]):
         raise NotImplementedError()
 
-    def __call__(self, corpus: List[str]):
+    def __call__(self, corpus: Union[List[str], str]):
         return self.tokenize(corpus)
 
 
@@ -22,17 +24,20 @@ class BERTTokenizer(BaseTokenizer):
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_path)
         self.padding = padding
 
-    def tokenize(self, corpus: List[str]):
+    def tokenize(self, corpus: Union[List[str], str]):
         return self.tokenizer(corpus, padding=self.padding, return_tensors='pt')
 
 
 class BiLSTMTokenizer(BaseTokenizer):
-    def __init__(self):
+    def __init__(self, vocab: Vocab):
         super().__init__()
         self.padding = True
-        self.vocab = None
+        self.vocab = vocab
 
-    def tokenize(self, corpus: List[str]):
+    def tokenize(self, corpus: Union[List[str], str]):
+        if isinstance(corpus, str):
+            corpus = [corpus]
+
         vectors = [self._vectorize_document(doc) for doc in corpus]
 
         if self.padding:
