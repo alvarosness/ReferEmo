2a4c82020f5f5f2f79a20ec861b8a3e3f7c87d8e
Alvaro Esperanca <alvaro.sdb.esperanca@gmail.com>
2021-09-15 14:58:33 -0400

    Add BiLSTM Tokenizer

--- models/tokenizers.py
+++ models/tokenizers.py
@@ -1,15 +1,18 @@
 from typing import List
+
+import torch
+from torch.nn.utils.rnn import pad_sequence
 from transformers import AutoTokenizer
 
 
 class BaseTokenizer:
     """Base tokenizer class. All tokenizers inherit from this base class"""
 
-    def tokenize(self, text: List[str]):
-        pass
+    def tokenize(self, corpus: List[str]):
+        raise NotImplementedError()
 
-    def __call__(self, text):
-        return self.tokenize(text)
+    def __call__(self, corpus: List[str]):
+        return self.tokenize(corpus)
 
 
 class BERTTokenizer(BaseTokenizer):
@@ -19,5 +22,25 @@ class BERTTokenizer(BaseTokenizer):
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_path)
         self.padding = padding
 
-    def tokenize(self, text: List[str]):
-        return self.tokenizer(text, padding=self.padding, return_tensors='pt')
+    def tokenize(self, corpus: List[str]):
+        return self.tokenizer(corpus, padding=self.padding, return_tensors='pt')
+
+
+class BiLSTMTokenizer(BaseTokenizer):
+    def __init__(self):
+        super().__init__()
+        self.padding = True
+        self.vocab = None
+
+    def tokenize(self, corpus: List[str]):
+        vectors = [self._vectorize_document(doc) for doc in corpus]
+
+        if self.padding:
+            return pad_sequence(vectors, padding_value=self.vocab['PAD'], batch_first=True)
+
+        return vectors
+
+    def _vectorize_document(self, doc: str):
+        vector = [self.vocab[word] for word in doc.split(' ')]
+
+        return torch.LongTensor(vector)
